329ded8afbfeb5538219b64e7c1256a51b5ee648
Yago Afonso <yago.afonso@hotmail.com>
2022-11-05 16:54:28 -0400

    feat: create pool

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -4,13 +4,36 @@ import logoImg  from '../assets/logo.svg'
 import usersAvatarExampleImge from '../assets/users-avatar-example.png'
 import iconCheckImg from '../assets/icon-check.svg'
 import { api } from '../lib/axios'
+import { FormEvent, useState } from 'react'
 
 interface homeProps {
   poolCount: number;
   guessCount: number;
   userCount: number;
 }
-export default function Home(props: homeProps) {    
+export default function Home(props: homeProps) {
+  const [poolTitle, setPoolTitle] = useState('')
+  
+  async function createPool(event: FormEvent) {
+    event.preventDefault()
+
+    try {
+      const response = await api.post('/pools', {
+        title: poolTitle,
+      });
+
+      const{ code } = response.data
+
+      await navigator.clipboard.writeText(code)
+
+      alert('Bolão criado com sucesso, o código foi copiado para a área de transferência!')
+      setPoolTitle('')
+    } catch (err) {
+      console.log(err)
+      alert('Falha ao criar o bolão, tente novamente!')
+    }
+  }
+
   return (
     <div className='max-w-[1124px] h-screen mx-auto grid grid-cols-2 gap-28 items-center'>
       <main>
@@ -27,13 +50,15 @@ export default function Home(props: homeProps) {
           </strong> 
         </div>
 
-        <form className='mt-10 flex gap-2'>
+        <form onSubmit={createPool} className='mt-10 flex gap-2'>
 
           <input 
-            className='flex-1 px-6 py-4 rounded bg-gray-800 border border-gray-600 text-sm'
+            className='flex-1 px-6 py-4 rounded bg-gray-800 border border-gray-600 text-sm text-gray-100'
             type="text" 
             required 
             placeholder="Qual nome do seu bolão?" 
+            onChange={event => setPoolTitle(event.target.value)}
+            value={poolTitle}
           />
           <button 
             className='bg-yellow-500 px-6 py-4 rounded text-gray-900 font-bold text-sm uppercase hover:bg-yellow-700'
